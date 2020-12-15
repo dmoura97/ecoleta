ad9d33ec0635ecf6fac6e9233d21d9c54f21b4f5
Danilo Moura <danilomouraa@icloud.com>
2020-12-15 17:40:28 -0300

    Criaçao das tabelas do banco de dados e listagem de items

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,12 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(express.json());
+app.use(routes);
 
-    response.json([
-        'Diego',
-        'Clayton',
-        'Robson',
-        'Daniel'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
